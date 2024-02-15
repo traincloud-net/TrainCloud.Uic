d8dd67fa00008859566793395b4c5403ef666d4c
Sebastian Hoyer <65442474+SebHoyer@users.noreply.github.com>
2024-02-15 11:48:42 +0100

    Update IUicNumberService.cs

--- TrainCloud.Uic/IUicNumberService.cs
+++ TrainCloud.Uic/IUicNumberService.cs
@@ -1,7 +1,7 @@
 ﻿namespace TrainCloud.Uic;
 
 /// <summary>
-/// Functions to work with UIC numbers
+/// Methods to work with UIC numbers
 /// </summary>
 public interface IUicNumberService
 {
@@ -11,7 +11,7 @@ public interface IUicNumberService
     /// <param name="fullUic">12 digit UIC number (e. g. 378478413297)</param>
     /// <returns>The 2 digit car type code of the provided UIC number (e. g. 37)</returns>
     /// <exception cref="ArgumentException">Thrown when the provided UIC number is invalid</exception>
-    public string GetCarTypeCode(string fullUic);
+    string GetCarTypeCode(string fullUic);
 
     /// <summary>
     /// Gets the 2 digit country code of the provided UIC number
@@ -19,7 +19,7 @@ public interface IUicNumberService
     /// <param name="fullUic">12 digit UIC number (e. g. 848047332772)</param>
     /// <returns>The 2 digit country code of the provided UIC number (e. g. 80)</returns>
     /// <exception cref="ArgumentException">Thrown when the provided UIC number is invalid</exception>
-    public string GetCountryCode(string fullUic);
+    string GetCountryCode(string fullUic);
 
     /// <summary>
     /// Gets the 4 digit car class code of the provided UIC number
@@ -27,7 +27,7 @@ public interface IUicNumberService
     /// <param name="fullUic">12 digit UIC number (e. g. 898075500014)</param>
     /// <returns>The 4 digit car class code of the provided UIC number (e. g. 7550)</returns>
     /// <exception cref="ArgumentException">Thrown when the provided UIC number is invalid</exception>
-    public string GetCarClassCode(string fullUic);
+    string GetCarClassCode(string fullUic);
 
     /// <summary>
     /// Gets the 3 digit car class serial number of the provided UIC number
@@ -35,7 +35,7 @@ public interface IUicNumberService
     /// <param name="fullUic">12 digit UIC number (e. g. 238064501006)</param>
     /// <returns>The 3 digit car class serial number of the provided UIC number (e. g. 100)</returns>
     /// <exception cref="ArgumentException">Thrown when the provided UIC number is invalid</exception>
-    public string GetCarClassSerialNr(string fullUic);
+    string GetCarClassSerialNr(string fullUic);
 
     /// <summary>
     /// Gets the 1 digit checksum of the provided valid UIC number
@@ -43,7 +43,7 @@ public interface IUicNumberService
     /// <param name="fullUic">12 digit UIC number (e. g. 235608390052)</param>
     /// <returns>The 1 digit checksum of the provided UIC number (e. g. 2)</returns>
     /// <exception cref="ArgumentException">Thrown when the provided UIC number is invalid</exception>
-    public string GetChecksum(string fullUic);
+    string GetChecksum(string fullUic);
 
     /// <summary>
     /// Checks if the provided UIC number is valid by calculating the checksum
