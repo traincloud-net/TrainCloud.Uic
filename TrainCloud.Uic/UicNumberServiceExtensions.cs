8e3e2dd8deed47cb7115f717206a1e8f18329c7d
Sebastian Hoyer <65442474+SebHoyer@users.noreply.github.com>
2024-03-20 22:19:10 +0100

    UicNumberServiceExtensions NuGet

---
+++ TrainCloud.Uic/UicNumberServiceExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace TrainCloud.Uic;
+
+public static class UicNumberServiceExtensions
+{
+    public static IServiceCollection AddUicNumberService(this IServiceCollection services)
+    {
+        services.AddScoped<IUicNumberService, UicNumberService>();
+
+        return services;
+    }
+}
